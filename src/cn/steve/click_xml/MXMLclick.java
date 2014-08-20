754f4f9e74ba04655963ba93d2c7f7cd66ac878d
tinggengyan <tinggengyan@gmail.com>
2014-08-20 17:50:49 +0800

    add the customer dialogue

---
+++ src/cn/steve/click_xml/MXMLclick.java
@@ -0,0 +1,25 @@
+package cn.steve.click_xml;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import cn.steve.study.R;
+
+public class MXMLclick extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.setContentView(R.layout.activity_click);
+
+	}
+	
+	//对应了按扭在文件中对应的函数名称
+	public void onClick(View v) {
+		//根据相应的控件的ID进行相应
+		if (v.getId() == R.id.textView) {
+			Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+}
---
+++ src/cn/steve/customaction/CustomerAction.java
@@ -0,0 +1,14 @@
+package cn.steve.customaction;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomerAction extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
