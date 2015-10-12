b89ad97e0d4765e3f5f31562b2d195b62fb25151
Steve <tinggengyan@gmail.com>
2015-10-12 18:43:45 +0800

    add gallery demo

--- app/src/main/java/cn/steve/gallery/GalleryMainActivity.java
+++ app/src/main/java/cn/steve/gallery/GalleryMainActivity.java
@@ -2,6 +2,7 @@ package cn.steve.gallery;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Gallery;
 
 import java.util.ArrayList;
@@ -22,6 +23,9 @@ public class GalleryMainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_main);
         gallery = (Gallery) findViewById(R.id.gallery);
+        gallery.setGravity(Gravity.CENTER_HORIZONTAL);      // 设置水平居中显示
+        gallery.setUnselectedAlpha(0.3f);                    // 设置未选中图片的透明度
+        gallery.setSpacing(40);                              // 设置图片之间的间距
         for (int i = 0; i < 365; i++) {
             datas.add("" + i);
         }
