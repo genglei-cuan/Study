7e01882e1025977715a287092032ade27824bd5c
Steve <tinggengyan@gmail.com>
2015-10-20 20:34:12 +0800

    add screen shot listen demo

---
+++ app/src/main/java/cn/steve/screenshot/ScreenShotActivity.java
@@ -0,0 +1,18 @@
+package cn.steve.screenshot;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.steve.study.R;
+
+/**
+ * Created by yantinggeng on 2015/10/20.
+ */
+public class ScreenShotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screenshot);
+    }
+}
