2e0463f32264d7e50e4f50810340c8cb389f990d
SteveYan <tinggengyan@gmail.com>
2015-10-25 22:50:54 +0800

    增加流失布局的demo

---
+++ app/src/main/java/cn/steve/flowlayout/FlowLayoutMainActivity.java
@@ -0,0 +1,23 @@
+package cn.steve.flowlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.steve.study.R;
+
+
+/**
+ * 流式布局的demo
+ *
+ * Created by Steve on 2015/8/26.
+ */
+public class FlowLayoutMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flowlayout_main);
+
+
+    }
+}
