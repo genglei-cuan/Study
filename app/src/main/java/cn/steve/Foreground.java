f2d72fa65f755ea02ad2f5fc2976581f20c4a7b2
Steve <tinggengyan@gmail.com>
2015-10-27 13:39:34 +0800

    修改foreground，前后台切换监听的类，修复不能监听restart的bug

--- app/src/main/java/cn/steve/Foreground.java
+++ app/src/main/java/cn/steve/Foreground.java
@@ -6,17 +6,11 @@ import android.app.Application;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 
 import java.lang.ref.WeakReference;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * 整个应用中的activity的生命周期的回调，借此可以判断有几个activity和是否还在前台运行， 借鉴了网上的一个demo，实现前后台切换时候的回调
- *
- * https://github.com/steveliles/Foredroid
+ * 整个应用中的activity的生命周期的回调，借此可以判断有几个activity和是否还在前台运行
  *
  * Created by yantinggeng on 2015/10/23.
  */
@@ -27,25 +21,10 @@ public class Foreground implements Application.ActivityLifecycleCallbacks {
 
     public static final String TAG = Foreground.class.getName();
     public static final long CHECK_DELAY = 2000;
-
+    public static ForeBackStateChangedListener listener;
+    public static Activity current;
     private static Foreground instance;
-
-    private static Callback becameForeground = new Callback() {
-        @Override
-        public void invoke(Listener listener) {
-            listener.onBecameForeground();
-        }
-    };
-    private static Callback becameBackground = new Callback() {
-        @Override
-        public void invoke(Listener listener) {
-            listener.onBecameBackground();
-        }
-    };
-
-    private boolean foreground;
-    private Activity current;
-    private Listeners listeners = new Listeners();
+    private static boolean foreground;
     private Handler handler = new Handler();
     private Runnable check;
 
@@ -72,28 +51,41 @@ public class Foreground implements Application.ActivityLifecycleCallbacks {
         return instance;
     }
 
-    public boolean isForeground() {
+    public static boolean isForeground() {
         return foreground;
     }
 
-    public boolean isBackground() {
+    public static boolean isBackground() {
         return !foreground;
-    }
-
-    public Binding addListener(Listener listener) {
-        return listeners.add(listener);
     }
 
     @Override
     public void onActivityResumed(Activity activity) {
+        current = activity;
+        //撤销已经存在的检查，此刻已经是重新启动了一个activity
+        if (check != null) {
+            handler.removeCallbacks(check);
+        }
+        //判断原先不在前台运行，并且没有旋转屏幕,表示切换到了前台
+        if (!foreground && (activity != null && !activity.isChangingConfigurations())) {
+            foreground = true;
+            listener.onBecameForeground();
+            System.out.println("became foreground");
+        }
+        //否则表示原先就在前台运行
+        else {
+            System.out.println("still foreground");
+        }
     }
 
     @Override
     public void onActivityPaused(Activity activity) {
-        // if we're changing configurations we aren't going background so
-        // no need to schedule the check
+        //全部的检查放到pause中
+        if (check != null) {
+            handler.removeCallbacks(check);
+        }
+        //只要不是旋转屏幕的原因,就进行检查是否已经退出了APP
         if (!activity.isChangingConfigurations()) {
-            // don't prevent activity being gc'd
             final WeakReference<Activity> ref = new WeakReference<>(activity);
             handler.postDelayed(check = new Runnable() {
                 @Override
@@ -102,33 +94,20 @@ public class Foreground implements Application.ActivityLifecycleCallbacks {
                 }
             }, CHECK_DELAY);
         }
+
     }
 
     @Override
     public void onActivityStarted(Activity activity) {
-        current = activity;
-        if (check != null) {
-            handler.removeCallbacks(check);
-        }
-        //判断原先不在前台运行，并且没有旋转屏幕,表示切换到了前台
-        if (!foreground && (activity != null && !activity.isChangingConfigurations())) {
-            foreground = true;
-            Log.w(TAG, "became foreground");
-            listeners.each(becameForeground);
-        }
-        //否则表示原先就在前台运行
-        else {
-            Log.i(TAG, "still foreground");
-        }
     }
 
     @Override
     public void onActivityStopped(Activity activity) {
         //移除检查
-        if (check != null) {
-            handler.removeCallbacks(check);
-        }
-        onActivityCeased(activity);
+//        if (check != null) {
+//            handler.removeCallbacks(check);
+//        }
+//        onActivityCeased(activity);
     }
 
     @Override
@@ -151,65 +130,26 @@ public class Foreground implements Application.ActivityLifecycleCallbacks {
             if ((activity == current) && (activity != null && !activity
                 .isChangingConfigurations())) {
                 foreground = false;
-                Log.w(TAG, "went background");
-                listeners.each(becameBackground);
+                listener.onBecameBackground();
+                System.out.println("went background");
             }
             //否则表示已经在前台
             else {
-                Log.i(TAG, "still foreground");
+                System.out.println("still foreground");
             }
         }
         //依旧在后台
         else {
-            Log.i(TAG, "still background");
+            System.out.println("still background");
         }
     }
 
-    public interface Listener {
+    //监听器,用作APP可见和不可见的时候的回调
+    public interface ForeBackStateChangedListener {
 
-        public void onBecameForeground();
+        void onBecameForeground();
 
-        public void onBecameBackground();
+        void onBecameBackground();
     }
 
-    public interface Binding {
-
-        void unbind();
-    }
-
-    private interface Callback {
-
-        void invoke(Listener listener);
-    }
-
-    private static class Listeners {
-
-        private List<WeakReference<Listener>> listeners = new CopyOnWriteArrayList<>();
-
-        public Binding add(Listener listener) {
-            final WeakReference<Listener> wr = new WeakReference<>(listener);
-            listeners.add(wr);
-            return new Binding() {
-                public void unbind() {
-                    listeners.remove(wr);
-                }
-            };
-        }
-
-        public void each(Callback callback) {
-            for (Iterator<WeakReference<Listener>> it = listeners.iterator(); it.hasNext(); ) {
-                try {
-                    WeakReference<Listener> wr = it.next();
-                    Listener l = wr.get();
-                    if (l != null) {
-                        callback.invoke(l);
-                    } else {
-                        it.remove();
-                    }
-                } catch (Exception exc) {
-                    Log.e(TAG, "Listener threw exception!", exc);
-                }
-            }
-        }
-    }
 }
