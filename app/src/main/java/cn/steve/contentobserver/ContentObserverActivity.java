085a3e84e4bfdc7da006e250b22cb24935e96e48
Steve <tinggengyan@gmail.com>
2015-11-05 15:58:45 +0800

    添加一个监听截屏的demo

---
+++ app/src/main/java/cn/steve/contentobserver/ContentObserverActivity.java
@@ -0,0 +1,69 @@
+package cn.steve.contentobserver;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+
+import cn.steve.study.R;
+import cn.trinea.android.common.util.FileUtils;
+
+/**
+ * 列出存储器中所有的截图文件
+ *
+ *
+ * Created by yantinggeng on 2015/11/5.
+ */
+public class ContentObserverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_textview);
+        getAllScreenShot();
+    }
+
+
+    //取出所有截图看看大小
+    private void getAllScreenShot() {
+        Cursor query;
+        String
+            str =
+            "( " + "_data" + " like ? or " + "_data" + " like ? or " + "_data" + " like ? )";
+        //查询数据库的内容
+        query = this.getContentResolver().query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, str,
+            new String[]{"%Screenshot%", "%screenshot%", "%\u622a\u5c4f%"},
+            MediaStore.Images.Media.DATE_ADDED);
+        if (query != null) {
+            try {
+                if (query.getCount() > 0) {
+                    query.moveToFirst();
+                    while (query.moveToNext()) {
+                        String
+                            realPath =
+                            query.getString(
+                                query.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+                        String type = query.getString(
+                            query.getColumnIndexOrThrow(MediaStore.Images.Media.MIME_TYPE));
+                        long size =
+                            query
+                                .getLong(query.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE));
+                        boolean hasType = !TextUtils.isEmpty(type);
+                        boolean isImage = type.contains("image") || type.contains("Image");
+                        //打印图片的详细信息
+                        System.out.println(realPath + "大小:" + size);
+                        System.out.println("真实大小:" + FileUtils.getFileSize(realPath));
+                    }
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        if (query != null) {
+            query.close();
+        }
+    }
+
+}
---
+++ app/src/main/java/cn/steve/contentobserver/ScreenShotContentObserver.java
@@ -0,0 +1,80 @@
+package cn.steve.contentobserver;
+
+import android.content.Context;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+
+import cn.trinea.android.common.util.FileUtils;
+
+/**
+ * 监听截屏产生的图片变化
+ *
+ * Created by yantinggeng on 2015/10/22.
+ */
+public class ScreenShotContentObserver extends ContentObserver {
+
+    private Context mContext;
+    private Handler mHandler;
+
+    public ScreenShotContentObserver(Context context, Handler handler) {
+        super(handler);
+        this.mContext = context;
+        this.mHandler = handler;
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+        super.onChange(selfChange);
+        Cursor query;
+        //检查四秒以内的变化
+        long currentTimeMillis = System.currentTimeMillis() / 1000;
+        String str = "date_added > " + (currentTimeMillis - 4)
+                     + " and " + "date_added" + " < "
+                     + (currentTimeMillis + 4) +
+                     " and ( " + "_data" + " like ? or " + "_data" + " like ? or " + "_data"
+                     + " like ? )";
+        //查询数据库的内容
+        query = this.mContext.getContentResolver().query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, str,
+            new String[]{"%Screenshot%", "%screenshot%", "%\u622a\u5c4f%"},
+            MediaStore.Images.Media.DATE_ADDED);
+
+        String[] columnNames = query.getColumnNames();
+        for (String s : columnNames) {
+            System.out.println("columnNames:" + s);
+        }
+
+        if (query != null) {
+            try {
+                if (query.getCount() > 0) {
+                    query.moveToLast();
+                    String
+                        realPath =
+                        query.getString(query.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+                    String type = query.getString(
+                        query.getColumnIndexOrThrow(MediaStore.Images.Media.MIME_TYPE));
+                    long size =
+                        query.getLong(query.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE));
+                    boolean hasType = !TextUtils.isEmpty(type);
+                    boolean isImage = type.contains("image") || type.contains("Image");
+                    if (size == 0) {
+                        //部分机型显示的所有值都是0
+                        size = FileUtils.getFileSize(realPath);
+                    }
+                    if (size > 1 && hasType && isImage) {
+                        //此处将截屏图片的绝对路径传递到handler处理
+                        mHandler.obtainMessage(1, realPath).sendToTarget();
+                    }
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        if (query != null) {
+            query.close();
+        }
+    }
+}
