05ed612d59348e69d8e7478e66458709196a518e
Steve <tinggengyan@gmail.com>
2015-11-10 15:40:44 +0800

    添加material design 的demo

---
+++ app/src/main/java/cn/steve/materialdesign/MaterialButtonActivity.java
@@ -0,0 +1,21 @@
+package cn.steve.materialdesign;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.steve.study.R;
+
+/**
+ * material design 的demo
+ *
+ * Created by yantinggeng on 2015/11/10.
+ */
+public class MaterialButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_button);
+
+    }
+}
