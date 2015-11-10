5fdc5eaa13f2c862c7a29539d2e74fc5057271f3
Steve <tinggengyan@gmail.com>
2015-11-10 18:14:41 +0800

    添加MVP架构体验的测试

---
+++ app/src/main/java/cn/steve/mvp/MVPMainActivity.java
@@ -0,0 +1,16 @@
+package cn.steve.mvp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yantinggeng on 2015/11/10.
+ */
+public class MVPMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
