d70df375aced4baf3abd3a0518d1543ae40a4eaa
Steve <tinggengyan@gmail.com>
2015-11-26 13:57:30 +0800

    添加滑动回弹的 scrollview

---
+++ app/src/main/java/cn/steve/pullscrollview/OverScrollViewActivity.java
@@ -0,0 +1,20 @@
+package cn.steve.pullscrollview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.steve.study.R;
+
+/**
+ * 滑动有回弹的效果
+ *
+ * Created by yantinggeng on 2015/11/26.
+ */
+public class OverScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overscrollview);
+    }
+}
