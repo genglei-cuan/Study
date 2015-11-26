02ed60347f71cd999d31d5c465c7b532e4c86aa2
Steve <tinggengyan@gmail.com>
2015-11-26 13:56:47 +0800

    添加material design的coordinatorlayout的demo

---
+++ app/src/main/java/cn/steve/pullscrollview/CoordinatorLayoutActivity.java
@@ -0,0 +1,19 @@
+package cn.steve.pullscrollview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.steve.study.R;
+
+/**
+ * Created by yantinggeng on 2015/11/26.
+ */
+public class CoordinatorLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinatorlayout);
+
+    }
+}
