43d01d7c63294400d43a8ade02b921f78836be0d
Steve <tinggengyan@gmail.com>
2015-12-02 20:38:02 +0800

    add pulltozoomviewdemo

---
+++ app/src/main/java/cn/steve/pulltozoomviewdemo/recyclerview/SpannableStaggeredGridLayoutManager.java
@@ -0,0 +1,15 @@
+package cn.steve.pulltozoomviewdemo.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by manishdeora on 14/05/15.
+ */
+public class SpannableStaggeredGridLayoutManager extends RecyclerView.LayoutManager{
+
+
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return null;
+    }
+}
