811cbf1238fb5a5cf53148bb81c0f880a931bd15
Steve <tinggengyan@gmail.com>
2015-12-23 18:11:04 +0800

    miss file

---
+++ app/src/main/java/cn/steve/viewresearch/viewResearchMainActivity.java
@@ -0,0 +1,18 @@
+package cn.steve.viewresearch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yantinggeng on 2015/12/22.
+ */
+public class ViewResearchMainActivity extends AppCompatActivity {
+
+    private static final String TAG = "viewResearchMainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
