cd61585c3e691a23dbbf6babdc47c3e6a6f2d788
Steve <tinggengyan@gmail.com>
2016-01-29 10:16:18 +0800

    add circleimageview demo

---
+++ app/src/main/java/cn/steve/circleimageview/CircleImageViewActivity.java
@@ -0,0 +1,18 @@
+package cn.steve.circleimageview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.steve.study.R;
+
+/**
+ * Created by yantinggeng on 2016/1/28.
+ */
+public class CircleImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circleimageview);
+    }
+}
