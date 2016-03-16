417d9c0c99da02960c92f87954661f49eeff0dc7
Steve <tinggengyan@gmail.com>
2016-03-16 13:08:24 +0800

    [zxing]  complete the demo

---
+++ app/src/main/java/cn/steve/zxing/ZxingActivity.java
@@ -0,0 +1,51 @@
+package cn.steve.zxing;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import cn.steve.study.R;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by yantinggeng on 2016/3/16.
+ */
+public class ZxingActivity extends AppCompatActivity {
+
+    private ImageView prescaleiamgeview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_imageview);
+        this.prescaleiamgeview = (ImageView) findViewById(R.id.prescale_iamgeview);
+        getCode();
+    }
+
+
+    void getCode() {
+        Observable.create(new Observable.OnSubscribe<Bitmap>() {
+            @Override
+            public void call(Subscriber<? super Bitmap> subscriber) {
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.codelogo);
+                subscriber.onNext(QRCodeUtil.createQRImage("http://www.baidu.com", 800, 800, bitmap));
+                subscriber.onCompleted();
+            }
+        }).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Action1<Bitmap>() {
+                @Override
+                public void call(Bitmap bitmap) {
+                    prescaleiamgeview.setImageBitmap(bitmap);
+                }
+            });
+    }
+
+
+}
