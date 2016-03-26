060a3b13a1a822d609e6a1c9979fedb357d7668f
SteveYan <tinggengyan@gmail.com>
2016-03-26 21:07:27 +0800

    [qrcode]   move the qrcode to my lib

--- app/src/main/java/cn/steve/zxing/ZxingActivity.java
+++ app/src/main/java/cn/steve/zxing/ZxingActivity.java
@@ -12,40 +12,41 @@ import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
+import steve.cn.mylib.qrcode.QRCodeUtil;
 
 /**
  * Created by yantinggeng on 2016/3/16.
  */
 public class ZxingActivity extends AppCompatActivity {
 
-    private ImageView prescaleiamgeview;
+  private ImageView prescaleiamgeview;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_imageview);
-        this.prescaleiamgeview = (ImageView) findViewById(R.id.prescale_iamgeview);
-        getCode();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_imageview);
+    this.prescaleiamgeview = (ImageView) findViewById(R.id.prescale_iamgeview);
+    getCode();
+  }
 
 
-    void getCode() {
-        Observable.create(new Observable.OnSubscribe<Bitmap>() {
-            @Override
-            public void call(Subscriber<? super Bitmap> subscriber) {
-                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.codelogo);
-                subscriber.onNext(QRCodeUtil.createQRImage("http://www.baidu.com", 800, 800, bitmap));
-                subscriber.onCompleted();
-            }
-        }).subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(new Action1<Bitmap>() {
-                @Override
-                public void call(Bitmap bitmap) {
-                    prescaleiamgeview.setImageBitmap(bitmap);
-                }
-            });
-    }
+  void getCode() {
+    Observable.create(new Observable.OnSubscribe<Bitmap>() {
+      @Override
+      public void call(Subscriber<? super Bitmap> subscriber) {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.codelogo);
+        subscriber.onNext(QRCodeUtil.createQRImage("http://www.baidu.com", 800, 800, bitmap));
+        subscriber.onCompleted();
+      }
+    }).subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Action1<Bitmap>() {
+          @Override
+          public void call(Bitmap bitmap) {
+            prescaleiamgeview.setImageBitmap(bitmap);
+          }
+        });
+  }
 
 
 }
