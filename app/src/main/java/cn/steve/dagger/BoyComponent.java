353c666bd7e1807b83c795389cb35f8401af55bb
SteveYan <tinggengyan@gmail.com>
2016-05-16 22:19:37 +0800

    dagger2 attemp to subcomponent

--- app/src/main/java/cn/steve/dagger/BoyComponent.java
+++ app/src/main/java/cn/steve/dagger/BoyComponent.java
@@ -8,16 +8,16 @@ import dagger.Component;
  * Created by Steve on 2016/5/10.
  */
 
-@Component(modules = BoysModule.class)
+@Component(dependencies = {MasterComponent.class}, modules = BoysModule.class)
 @PerBoy
 public interface BoyComponent {
 
-    //依赖于boys的component需要这个对象，这样做的目标是向外暴露，缺少就会报错
-    //所以说，component模块化之后，需要被依赖的部分必须显性的声明，表示提供给上层依赖
+  //依赖于boys的component需要这个对象，这样做的目标是向外暴露，缺少就会报错
+  //所以说，component模块化之后，需要被依赖的部分必须显性的声明，表示提供给上层依赖
 
-    @Named("name")
-    Boy boyName();
+  @Named("name")
+  Boy boyName();
 
-    @Named("noname")
-    Boy boy();
+  @Named("noname")
+  Boy boy();
 }
--- app/src/main/java/cn/steve/dagger/MasterComponent.java
+++ app/src/main/java/cn/steve/dagger/MasterComponent.java
@@ -5,10 +5,16 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 /**
+ * 现在的需求是想创建一个全局的对象，不考虑Android的特殊性，
+ *
+ *
  * Created by yantinggeng on 2016/5/13.
  */
 @Singleton
 @Component(modules = MasterModule.class)
 public interface MasterComponent {
+
+  @Singleton
+  Master master();
 
 }
