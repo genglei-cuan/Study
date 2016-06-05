0c634c061f40237d197ec173a5a8c0be5df4e22e
SteveYan <tinggengyan@gmail.com>
2016-06-05 22:31:09 +0800

    [pullzoomview] pull to enlarge the head view todo

---
+++ app/src/main/java/cn/steve/pulltozoomviewdemo/MyPullZoomViewDemo.java
@@ -0,0 +1,21 @@
+package cn.steve.pulltozoomviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import cn.steve.study.R;
+import steve.cn.mylib.view.PullZoomView;
+
+public class MyPullZoomViewDemo extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_my_pull_zoom_view_demo);
+    PullZoomView pullZoomView = (PullZoomView) findViewById(R.id.pullZoomView);
+    ImageView headImageView = (ImageView) findViewById(R.id.headImageView);
+    pullZoomView.setHeadView(headImageView);
+    pullZoomView.setMaxZoomDelta(100);
+  }
+}
---
+++ mylib/src/main/java/steve/cn/mylib/view/PullZoomView.java
@@ -0,0 +1,86 @@
+package steve.cn.mylib.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * 做一个仿造携程下拉放大头部的view，向上滚动一起滚动的控件
+ *
+ * Created by Steve on 2016/6/5.
+ */
+public class PullZoomView extends LinearLayout {
+
+  private View headView;
+  private float headHeight;
+  private float touchY;
+  private float lastTouchY;
+  private float maxZoomDelta;
+  private boolean isMax = false;
+
+  public PullZoomView(Context context) {
+    super(context);
+  }
+
+  public PullZoomView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+    return false;
+//    return super.onInterceptTouchEvent(ev);
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    float y = event.getY();
+    int action = event.getAction();
+    switch (action) {
+      case MotionEvent.ACTION_DOWN:
+        lastTouchY = y;
+        break;
+
+      case MotionEvent.ACTION_MOVE:
+        float delta = y - lastTouchY;
+        if (delta > 0) {
+          if (delta < maxZoomDelta) {
+            setHeadViewHeight(delta);
+          } else {
+            isMax = true;
+          }
+        }
+        break;
+      case MotionEvent.ACTION_UP:
+        if (isMax) {
+          setHeadViewHeight(-maxZoomDelta);
+        } else {
+          float d = lastTouchY - y;
+          setHeadViewHeight(d);
+        }
+        break;
+    }
+    return super.onTouchEvent(event);
+  }
+
+
+  private void setHeadViewHeight(float delta) {
+    ViewGroup.LayoutParams layoutParams = headView.getLayoutParams();
+    layoutParams.height += delta;
+    headView.setLayoutParams(layoutParams);
+  }
+
+  public void setHeadView(View headView) {
+    this.headView = headView;
+  }
+
+  public void setMaxZoomDelta(float maxZoomDelta) {
+    this.maxZoomDelta = maxZoomDelta;
+  }
+
+
+}
