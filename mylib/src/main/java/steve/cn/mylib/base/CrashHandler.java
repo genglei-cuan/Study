831a52d14efb490b456e887fe7c1686e6069d1b4
Steve <tinggengyan@gmail.com>
2016-07-04 17:25:31 +0800

    [note]  add a tip and crash handler

---
+++ mylib/src/main/java/steve/cn/mylib/base/CrashHandler.java
@@ -0,0 +1,104 @@
+package steve.cn.mylib.base;
+
+import android.content.Context;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * Created by yantinggeng on 2016/7/4.
+ */
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    private static final String TAG = CrashHandler.class.getSimpleName();
+    private static CrashHandler instance; // 单例模式
+    private Context context; // 程序Context对象
+    private Thread.UncaughtExceptionHandler defalutHandler; // 系统默认的UncaughtException处理类
+    private DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss.SSS", Locale.CHINA);
+
+    private CrashHandler() {
+    }
+
+    /**
+     * 获取CrashHandler实例
+     *
+     * @return CrashHandler
+     */
+    public static CrashHandler getInstance() {
+        if (instance == null) {
+            synchronized (CrashHandler.class) {
+                if (instance == null) {
+                    instance = new CrashHandler();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    /**
+     * 异常处理初始化
+     */
+    public void init(Context context) {
+        this.context = context;
+        // 获取系统默认的UncaughtException处理器
+        defalutHandler = Thread.getDefaultUncaughtExceptionHandler();
+        // 设置该CrashHandler为程序的默认处理器
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+
+    /**
+     * 当UncaughtException发生时会转入该函数来处理
+     */
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+        // 自定义错误处理
+        boolean res = handleException(ex);
+        if (!res && defalutHandler != null) {
+            // 如果用户没有处理则让系统默认的异常处理器来处理
+            defalutHandler.uncaughtException(thread, ex);
+
+        } else {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Log.e(TAG, "error : ", e);
+            }
+            // 退出程序
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
+     *
+     * @return true:如果处理了该异常信息;否则返回false.
+     */
+    private boolean handleException(final Throwable ex) {
+        if (ex == null) {
+            return false;
+        }
+        new Thread() {
+            @Override
+            public void run() {
+                Looper.prepare();
+
+                ex.printStackTrace();
+                String err = "[" + ex.getMessage() + "]";
+                Toast.makeText(context, "程序出现异常." + err, Toast.LENGTH_LONG)
+                    .show();
+
+                Looper.loop();
+            }
+
+        }.start();
+        // TODO 保存日志文件
+
+        return true;
+    }
+}
---
+++ notes/tips/tip1.md
@@ -0,0 +1,12 @@
+# 开发小Tip1
+
+## 查看activity启动的时间
+
+之前想查看整个activity的启动时间都是去在代码中手动记录时间，在oncreate和onresume处记录时间，转换一下，打印出log。
+其实系统每次启动一个activity，加载一个activity都会打印日志，直接过滤Displayed即可。
+
+```java
+07-04 14:04:25.066 925-965/? I/ActivityManager: Displayed com.android.settings/.applications.InstalledAppDetails: +1s57ms
+07-04 14:04:45.770 925-965/? I/ActivityManager: Displayed com.gift.android/.activity.MainActivity: +1s806ms
+```
+后面的时间就是该activity的启动时间，当然如果你用ActivityManager过滤的话可以看到更加详细的细节，甚至发现一些不可思议的东西，比如，我发现，某些我未安装的软件的包名。哈哈。
