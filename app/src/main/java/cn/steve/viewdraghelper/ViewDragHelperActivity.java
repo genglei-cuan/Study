8a4ec63f5152d99bac05acc450ceeae1e3a1c09b
Steve <tinggengyan@gmail.com>
2016-07-05 20:28:34 +0800

    [view drag helper]  add a drag view helper

---
+++ app/src/main/java/cn/steve/viewdraghelper/ViewDragHelperActivity.java
@@ -0,0 +1,65 @@
+package cn.steve.viewdraghelper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import cn.steve.study.R;
+import steve.cn.mylib.view.YoutubeLayout;
+
+/**
+ * Created by yantinggeng on 2016/7/5.
+ */
+public class ViewDragHelperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+
+        final TextView viewHeader = (TextView) findViewById(R.id.header);
+        final YoutubeLayout youtubeLayout = (YoutubeLayout) findViewById(R.id.dragLayout);
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                viewHeader.setText(listView.getAdapter().getItem(i).toString());
+                youtubeLayout.setVisibility(View.VISIBLE);
+                youtubeLayout.maximize();
+            }
+        });
+
+        listView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 50;
+            }
+
+            @Override
+            public String getItem(int i) {
+                return "object" + i;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return i;
+            }
+
+            @Override
+            public View getView(int i, View rView, ViewGroup viewGroup) {
+                View view = rView;
+                if (view == null) {
+                    view = getLayoutInflater().inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+                }
+                ((TextView) view.findViewById(android.R.id.text1)).setText(getItem(i));
+                return view;
+            }
+        });
+    }
+}
--- mylib/src/main/java/steve/cn/mylib/view/PullZoomView.java
+++ mylib/src/main/java/steve/cn/mylib/view/PullZoomView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
@@ -14,73 +15,79 @@ import android.widget.LinearLayout;
  */
 public class PullZoomView extends LinearLayout {
 
-  private View headView;
-  private float headHeight;
-  private float touchY;
-  private float lastTouchY;
-  private float maxZoomDelta;
-  private boolean isMax = false;
+    private View headView;
+    private float headHeight;
+    private float touchY;
+    private float lastTouchY;
+    private float maxZoomDelta;
+    private boolean isMax = false;
+    //最小滑动距离
+    private int scaledTouchSlop;
 
-  public PullZoomView(Context context) {
-    super(context);
-  }
-
-  public PullZoomView(Context context, AttributeSet attrs) {
-    super(context, attrs);
-  }
-
-  @Override
-  public boolean onInterceptTouchEvent(MotionEvent ev) {
-
-    return false;
-//    return super.onInterceptTouchEvent(ev);
-  }
-
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-    float y = event.getY();
-    int action = event.getAction();
-    switch (action) {
-      case MotionEvent.ACTION_DOWN:
-        lastTouchY = y;
-        break;
-
-      case MotionEvent.ACTION_MOVE:
-        float delta = y - lastTouchY;
-        if (delta > 0) {
-          if (delta < maxZoomDelta) {
-            setHeadViewHeight(delta);
-          } else {
-            isMax = true;
-          }
-        }
-        break;
-      case MotionEvent.ACTION_UP:
-        if (isMax) {
-          setHeadViewHeight(-maxZoomDelta);
-        } else {
-          float d = lastTouchY - y;
-          setHeadViewHeight(d);
-        }
-        break;
+    public PullZoomView(Context context) {
+        super(context);
     }
-    return super.onTouchEvent(event);
-  }
+
+    public PullZoomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private void init() {
+        scaledTouchSlop = ViewConfiguration.get(this.getContext()).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        return false;
+//    return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float y = event.getY();
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                lastTouchY = y;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                float delta = y - lastTouchY;
+                if (delta > 0) {
+                    if (delta < maxZoomDelta) {
+                        setHeadViewHeight(delta);
+                    } else {
+                        isMax = true;
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isMax) {
+                    setHeadViewHeight(-maxZoomDelta);
+                } else {
+                    float d = lastTouchY - y;
+                    setHeadViewHeight(d);
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
 
 
-  private void setHeadViewHeight(float delta) {
-    ViewGroup.LayoutParams layoutParams = headView.getLayoutParams();
-    layoutParams.height += delta;
-    headView.setLayoutParams(layoutParams);
-  }
+    private void setHeadViewHeight(float delta) {
+        ViewGroup.LayoutParams layoutParams = headView.getLayoutParams();
+        layoutParams.height += delta;
+        headView.setLayoutParams(layoutParams);
+    }
 
-  public void setHeadView(View headView) {
-    this.headView = headView;
-  }
+    public void setHeadView(View headView) {
+        this.headView = headView;
+    }
 
-  public void setMaxZoomDelta(float maxZoomDelta) {
-    this.maxZoomDelta = maxZoomDelta;
-  }
+    public void setMaxZoomDelta(float maxZoomDelta) {
+        this.maxZoomDelta = maxZoomDelta;
+    }
 
 
 }
