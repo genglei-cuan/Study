56b263b95b95bdfbab02886727633f41c4c922d0
steveyan <tinggengyan@gmail.com>
2016-07-13 01:28:38 +0800

    [TaskStack] TaskStack

--- app/src/main/java/cn/steve/notification/TaskStackActivity.java
+++ app/src/main/java/cn/steve/notification/TaskStackActivity.java
@@ -4,13 +4,11 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-
-import cn.steve.mvp.campaign.CampaignActivity;
+import android.support.v4.app.TaskStackBuilder;
 
 /**
  * 点击notification进入一个activity界面，按下返回键的时候，回到主界面，类似于QQ
@@ -24,17 +22,20 @@ public class TaskStackActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // create an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+        // Sets the Activity to start in a new, empty task
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(ResultActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
         mBuilder.setSmallIcon(android.R.drawable.ic_dialog_email);
         mBuilder.setContentTitle("My Notification!");
         mBuilder.setContentText("Hello World!");
 
-        // create an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, ResultActivity.class);
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(CampaignActivity.class);
-        stackBuilder.addNextIntent(resultIntent);
 
         PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(resultPendingIntent);
