5447462d8c8c0564e9d0d894002005af31c70b87
yantinggeng <tinggengyan@gmail.com>
2016-08-11 11:07:11 +0800

    [dialog ] add  shake animation

--- app/src/main/java/cn/steve/alertDialog/AlertDialogActivity.java
+++ app/src/main/java/cn/steve/alertDialog/AlertDialogActivity.java
@@ -2,10 +2,14 @@ package cn.steve.alertDialog;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -26,9 +30,11 @@ public class AlertDialogActivity extends Activity {
         setContentView(R.layout.activity_main_button);
         Button btn = (Button) findViewById(R.id.buttonMain);
         btn.setOnClickListener(new OnClickListener() {
+            View contentView = null;
+
             @Override
             public void onClick(View arg0) {
-                singleChooseDialog();
+                newDialog();
             }
 
             private void singleChooseDialog() {
@@ -60,7 +66,19 @@ public class AlertDialogActivity extends Activity {
             }
 
             private void newDialog() {
-
+                contentView = LayoutInflater.from(AlertDialogActivity.this).inflate(R.layout.activity_main_imageview, null);
+                contentView.findViewById(R.id.prescale_iamgeview).setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Animation shake = AnimationUtils.loadAnimation(AlertDialogActivity.this, R.anim.shake);//加载动画资源文件
+                        if (contentView != null) {
+                            contentView.startAnimation(shake);
+                        }
+                    }
+                });
+                Dialog dialog = new Dialog(AlertDialogActivity.this);
+                dialog.setContentView(contentView);
+                dialog.show();
             }
 
         });
