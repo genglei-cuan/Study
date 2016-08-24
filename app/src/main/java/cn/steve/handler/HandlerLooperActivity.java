986753bd5e4475b3026e66653383e6874ea12600
steveyan <tinggengyan@gmail.com>
2016-08-24 23:07:20 +0800

    [looper ] add  looper demo

--- app/src/main/java/cn/steve/handler/HandlerLooperActivity.java
+++ app/src/main/java/cn/steve/handler/HandlerLooperActivity.java
@@ -6,6 +6,7 @@ import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import cn.steve.study.R;
 
@@ -17,12 +18,15 @@ public class HandlerLooperActivity extends AppCompatActivity {
     private static final String TAG = "HandlerLooperActivity";
 
     private Handler handler = new Handler();
+    private TextView textViewMain;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_textview);
-        Log.e(TAG, "onCreate: " + handler.getLooper().toString());
+        textViewMain = (TextView) findViewById(R.id.textViewMain);
+        Log.e(TAG, "onCreate: getLooper():" + handler.getLooper().toString());
+        Log.e(TAG, "onCreate: Thread :" + Thread.currentThread());
         newLooper4Thread();
     }
 
@@ -30,12 +34,17 @@ public class HandlerLooperActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                Log.e(TAG, "run: Thread before prepare():" + Thread.currentThread());
                 Looper.prepare();
                 Handler handler = new Handler();
                 String s = handler.getLooper().toString();
-                Log.e(TAG, "run: Thread" + s);
-                Log.e(TAG, "run: MainLooper" + Looper.getMainLooper().toString());
-                Log.e(TAG, "run: myLooper" + Looper.myLooper().toString());
+                Log.e(TAG, "run: Thread getLooper():" + s);
+                Log.e(TAG, "run: Thread getMainLooper():" + Looper.getMainLooper().toString());
+                Log.e(TAG, "run: Thread myLooper():" + Looper.myLooper().toString());
+                Log.e(TAG, "run: Thread between prepare():" + Thread.currentThread());
+                textViewMain.setText(Thread.currentThread().toString());
+                Looper.loop();
+                Log.e(TAG, "run: Thread after prepare():" + Thread.currentThread());
             }
         }).start();
     }
