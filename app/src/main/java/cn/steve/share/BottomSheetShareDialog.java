884d3e61afd57d348f8f63c5a7f50e050058d0b8
steveyan <tinggengyan@gmail.com>
2016-10-07 16:02:32 +0800

    [share] add new sharedialog

---
+++ app/src/main/java/cn/steve/share/BottomSheetShareDialog.java
@@ -0,0 +1,139 @@
+package cn.steve.share;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatDialog;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.FrameLayout;
+
+/**
+ * Created by steveyan on 16-10-7.
+ */
+
+public class BottomSheetShareDialog extends AppCompatDialog {
+
+    private int defaultState = BottomSheetBehavior.STATE_EXPANDED;
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetCallback
+        = new BottomSheetBehavior.BottomSheetCallback() {
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet,
+                                   @BottomSheetBehavior.State int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+
+
+    public BottomSheetShareDialog(@NonNull Context context) {
+        this(context, 0);
+    }
+
+    public BottomSheetShareDialog(@NonNull Context context, @StyleRes int theme) {
+        super(context, getThemeResId(context, theme));
+        // We hide the title bar for any style configuration. Otherwise, there will be a gap
+        // above the bottom sheet when it is expanded.
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    protected BottomSheetShareDialog(@NonNull Context context, boolean cancelable, OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    private static int getThemeResId(Context context, int themeId) {
+        if (themeId == 0) {
+            // If the provided theme is 0, then retrieve the dialogTheme from our theme
+            TypedValue outValue = new TypedValue();
+            if (context.getTheme().resolveAttribute(
+                android.support.design.R.attr.bottomSheetDialogTheme, outValue, true)) {
+                themeId = outValue.resourceId;
+            } else {
+                // bottomSheetDialogTheme is not provided; we default to our light theme
+                themeId = android.support.design.R.style.Theme_Design_Light_BottomSheetDialog;
+            }
+        }
+        return themeId;
+    }
+
+    public void setDefaultState(int defaultState) {
+        this.defaultState = defaultState;
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResId) {
+        super.setContentView(wrapInBottomSheet(layoutResId, null, null));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(wrapInBottomSheet(0, view, null));
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        super.setContentView(wrapInBottomSheet(0, view, params));
+    }
+
+    private View wrapInBottomSheet(int layoutResId, View view, ViewGroup.LayoutParams params) {
+        final CoordinatorLayout coordinator = (CoordinatorLayout) View.inflate(getContext(), android.support.design.R.layout.design_bottom_sheet_dialog, null);
+        if (layoutResId != 0 && view == null) {
+            view = getLayoutInflater().inflate(layoutResId, coordinator, false);
+        }
+        FrameLayout bottomSheet = (FrameLayout) coordinator.findViewById(android.support.design.R.id.design_bottom_sheet);
+        BottomSheetBehavior<FrameLayout> bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setState(defaultState);
+        bottomSheetBehavior.setBottomSheetCallback(mBottomSheetCallback);
+        if (params == null) {
+            bottomSheet.addView(view);
+        } else {
+            bottomSheet.addView(view, params);
+        }
+        // We treat the CoordinatorLayout as outside the dialog though it is technically inside
+        if (shouldWindowCloseOnTouchOutside()) {
+            coordinator.findViewById(android.support.design.R.id.touch_outside).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (isShowing()) {
+                            cancel();
+                        }
+                    }
+                });
+        }
+        return coordinator;
+    }
+
+    private boolean shouldWindowCloseOnTouchOutside() {
+        if (Build.VERSION.SDK_INT < 11) {
+            return true;
+        }
+        TypedValue value = new TypedValue();
+        //noinspection SimplifiableIfStatement
+        if (getContext().getTheme()
+            .resolveAttribute(android.R.attr.windowCloseOnTouchOutside, value, true)) {
+            return value.data != 0;
+        }
+        return false;
+    }
+
+}
--- app/src/main/java/cn/steve/share/BottomSheetShareFragment.java
+++ app/src/main/java/cn/steve/share/BottomSheetShareFragment.java
@@ -1,18 +1,17 @@
 package cn.steve.share;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
-import android.support.design.widget.BottomSheetDialog;
-import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 
@@ -22,7 +21,7 @@ import cn.steve.study.R;
 /**
  * Created by yantinggeng on 2016/4/11.
  */
-public class BottomSheetShareFragment extends BottomSheetDialogFragment {
+public class BottomSheetShareFragment extends AppCompatDialogFragment {
 
     public static final String SHAREITEMS = "SHAREITEMS";
 
@@ -48,6 +47,7 @@ public class BottomSheetShareFragment extends BottomSheetDialogFragment {
 
         final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.bottomSheetShareRecyclerView);
         recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
         int spacing = getResources().getDimensionPixelSize(R.dimen.space_14);
         int spanCount = 4;
         recyclerView.addItemDecoration(new GridSpacingItemDecoration(spanCount, spacing, true));
@@ -74,22 +74,16 @@ public class BottomSheetShareFragment extends BottomSheetDialogFragment {
         return view;
     }
 
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
-        bottomSheetDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                FrameLayout bottomSheet = (FrameLayout) bottomSheetDialog.findViewById(android.support.design.R.id.design_bottom_sheet);
-                assert bottomSheet != null;
-                BottomSheetBehavior.from(bottomSheet).setState(BottomSheetBehavior.STATE_EXPANDED);
-            }
-        });
-        return bottomSheetDialog;
-    }
-
     public void setPresenter(ISharePresenter presenter) {
         this.presenter = presenter;
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        BottomSheetShareDialog bottomSheetShareDialog = new BottomSheetShareDialog(getActivity(), getTheme());
+        bottomSheetShareDialog.setDefaultState(BottomSheetBehavior.STATE_EXPANDED);
+        return bottomSheetShareDialog;
     }
 }
