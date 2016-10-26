7d2a6bb0f4f9bd5b63ab1cdecad2bfb4dd7186ac
yantinggeng <tinggengyan@gmail.com>
2016-10-27 01:46:24 +0800

    [list date piker]  add group divider line

--- app/src/main/java/cn/steve/dateCalendar/basic/DividerGridItemDecoration.java
+++ app/src/main/java/cn/steve/dateCalendar/basic/DividerGridItemDecoration.java
@@ -7,10 +7,13 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 /**
+ * 这里的分割线处理的不好，目前的处理方案是对group设置上下两条分割线，对于一组item结束设置分割线的情况，目前发现可能是由于复用的原因，似乎不能正常的显示。
+ *
  * Created by yantinggeng on 2016/10/26.
  */
 
@@ -46,7 +49,7 @@ public class DividerGridItemDecoration extends RecyclerView.ItemDecoration {
         int childCount = parent.getChildCount();
         for (int i = 0; i < childCount; i++) {
             final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+            //final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
             //final int left = child.getLeft() - params.leftMargin;
             //final int right = child.getRight() + params.rightMargin + mDivider.getIntrinsicWidth();
             //final int top = child.getBottom() + params.bottomMargin;
@@ -57,6 +60,13 @@ public class DividerGridItemDecoration extends RecyclerView.ItemDecoration {
             int top = child.getBottom();
             int bottom = top + mDivider.getIntrinsicHeight();
 
+            Log.e("MonthEnd", "drawHorizontalLine: " + String.valueOf(isMonthLast(child, parent)));
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+
+            bottom = child.getTop();
+            top = bottom - mDivider.getIntrinsicHeight();
             mDivider.setBounds(left, top, right, bottom);
             mDivider.draw(c);
         }
@@ -72,25 +82,31 @@ public class DividerGridItemDecoration extends RecyclerView.ItemDecoration {
     }
 
     private boolean isMonthLast(View view, RecyclerView parent) {
-        int childCount = parent.getChildCount();
-        int position = parent.getChildLayoutPosition(view);
-        if (position + 1 == childCount) {
+        int childCount = parent.getAdapter().getItemCount();
+        int position = parent.getChildAdapterPosition(view);
+        if (position + 1 == childCount || position + 2 == childCount || position + 3 == childCount || position + 4 == childCount
+            || position + 5 == childCount || position + 6 == childCount || position + 7 == childCount) {
             return true;
         }
-        View next = parent.getChildAt(position + 1);
-        return next != null && isTitle(next);
+        for (int i = 1; i < 8; i++) {
+            View next = parent.getChildAt(position + i);
+            if (next == null) {
+                break;
+            }
+            boolean is = isTitle(next);
+            if (is) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         // 如果是最后一行，则不需要绘制底部
         boolean isTitle = isTitle(view);
-
-        int childAdapterPosition = parent.getChildAdapterPosition(view);
-
         if (isTitle) {
-            System.out.println("Title:" + childAdapterPosition);
-            outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
+            outRect.set(0, mDivider.getIntrinsicHeight(), 0, mDivider.getIntrinsicHeight());
         } else {
             super.getItemOffsets(outRect, view, parent, state);
         }
